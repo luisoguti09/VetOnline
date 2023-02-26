e3c91f881f275f3f204bcb5e9c655bc0cd8aa106
luisoguti09 <luisoguti09@gmail.com>
2023-02-25 23:27:40 -0300

    json vet subido

--- src/app/veterinarios/veterinarios.page.ts
+++ src/app/veterinarios/veterinarios.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-veterinarios',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VeterinariosPage implements OnInit {
 
-  constructor() { }
+  vets: any = [];
+
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+  ) { }
 
   ngOnInit() {
+
+    this.getVeterinarios().subscribe(res =>{
+      console.log("Res", res)
+      this.vets = res; 
+    });
+
+  }
+
+  getVeterinarios(){
+    return this.http
+    .get("assets/files/veterinarios.json")
+    .pipe(
+      map((res: any) =>{
+        return res.data;
+      }
+      ));
   }
 
 }
